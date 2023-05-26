71317a0657d41cd2f64bfcbcf15098c82d16f3a3
SameerFaisal <sameerfaisal46@gmail.com>
2023-05-26 17:09:21 +0500

    2211E2 Revision 26-05-2023

---
+++ app.js
@@ -0,0 +1,68 @@
+// function display(){
+//     document.write("Hello World")
+// }
+
+// display()
+// display()
+// display()
+
+// const display=()=>{
+//     document.write("Hello World")
+// }
+
+// display()
+// display()
+// let user=+prompt("Enter an alphabet")
+// function isVowel(input){
+// input=parseFloat(input)+parseFloat(input)
+// console.log(input)
+// }
+// isVowel(user)
+// console.log(user)
+
+
+
+
+
+
+// function multiply(num1,num2,num3=2){
+// let result=num1*num2*num3
+// console.log(result)
+// }
+
+// let num1=+prompt()
+// let num2=+prompt()
+// multiply(num1,num2)
+
+
+// function greet(){
+//     let fname=document.getElementById("fn").value
+//     let lname=document.getElementById("ln").value
+//     let greeting="Hello "+fname+" "+lname
+//     document.getElementById("output").innerHTML=greeting
+// }
+
+// const add=()=>{
+//     let fn=document.getElementById("fn").value
+//     let sn=document.getElementById("sn").value
+//     let result=parseFloat(fn)+parseFloat(sn)
+//     document.getElementById("output").innerHTML=result
+// }
+// const sub=()=>{
+//     let fn=document.getElementById("fn").value
+//     let sn=document.getElementById("sn").value
+//     let result=parseFloat(fn)-parseFloat(sn)
+//     document.getElementById("output").innerHTML=result
+// }
+// const mul=()=>{
+//     let fn=document.getElementById("fn").value
+//     let sn=document.getElementById("sn").value
+//     let result=parseFloat(fn)*parseFloat(sn)
+//     document.getElementById("output").innerHTML=result
+// }
+// const div=()=>{
+//     let fn=document.getElementById("fn").value
+//     let sn=document.getElementById("sn").value
+//     let result=parseFloat(fn)/parseFloat(sn)
+//     document.getElementById("output").innerHTML=result
+// }
